c8c303f4d021fbe6e96037c9093a88528dcaa083
replaceitem <40722305+replaceitem@users.noreply.github.com>
2021-05-25 18:35:28 +0200

    Made offset dynamic

--- src/main/java/carpet/mixins/ChatHud_chatUpMixin.java
+++ src/main/java/carpet/mixins/ChatHud_chatUpMixin.java
@@ -1,7 +1,11 @@
 package carpet.mixins;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -10,7 +14,16 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(ChatHud.class)
 public class ChatHud_chatUpMixin
 {
-    private static final int CHAT_UP_OFFSET = 10;
+    @Shadow @Final private MinecraftClient client;
+
+    private int getOffset()
+    {
+        ClientPlayerEntity player = this.client.player;
+        if(player == null || player.isCreative() || player.isSpectator()) return 0;
+        int offset = player.getArmor()>0?10:0;
+        if(player.getAbsorptionAmount()>0) offset += 10;
+        return offset;
+    }
 
     @ModifyArg(method = "render", index = 1, at = @At(
             value = "INVOKE",
@@ -19,12 +32,12 @@ public class ChatHud_chatUpMixin
     ))
     private float offsetY(float y)
     {
-        return y- CHAT_UP_OFFSET;
+        return y - getOffset();
     }
 
     @ModifyConstant(method = "getText", constant = @Constant(doubleValue = 40.0), expect = 1)
     private double textBottomOffset(double original)
     {
-        return original+ CHAT_UP_OFFSET;
+        return original + getOffset();
     }
 }
