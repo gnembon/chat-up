1328d5d373be8e922761a2dfbd8380ea7609b123
spjoes <34802651+spjoes@users.noreply.github.com>
2021-06-10 02:22:09 -0400

    Added TinyConfig support. (TODO: re-add Dynamic Absorption Hearts)

--- src/main/java/carpet/mixins/ChatHud_chatUpMixin.java
+++ src/main/java/carpet/mixins/ChatHud_chatUpMixin.java
@@ -1,7 +1,12 @@
 package carpet.mixins;
 
+import carpet.config.Config;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -10,7 +15,37 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(ChatHud.class)
 public class ChatHud_chatUpMixin
 {
-    private static final int CHAT_UP_OFFSET = 10;
+
+    @Shadow @Final private MinecraftClient client;
+
+    private int getOffset()
+    {
+        ClientPlayerEntity player = this.client.player;
+        if(player == null || player.isCreative() || player.isSpectator()) return 0;
+
+        if(Config.disableMod == true) {
+            return 0;
+        } else {
+
+            if(Config.dynamicOffset == true) {
+                if (player.getArmor() > 0) {
+                    return Config.extraOffset + 10;
+                } else {
+                    return Config.extraOffset;
+                }
+            } else if(Config.dynamicOffset == false) {
+                int offset = Config.extraOffset + 10;
+                return offset;
+
+            //TODO: Fix Absorbtion so it gets effected by ANY offset (not just extraOffset)
+//                if (player.getAbsorptionAmount()>0){
+//                    int offset =+ Config.extraOffset;
+//                    return offset;
+//                }
+            }
+        }
+        return 0;
+    }
 
     @ModifyArg(method = "render", index = 1, at = @At(
             value = "INVOKE",
@@ -19,12 +54,12 @@ public class ChatHud_chatUpMixin
     ))
     private double offsetY(double y)
     {
-        return y- CHAT_UP_OFFSET;
+        return y - getOffset();
     }
 
     @ModifyConstant(method = "getText", constant = @Constant(doubleValue = 40.0))
     private double textBottomOffset(double original)
     {
-        return original+ CHAT_UP_OFFSET;
+        return original + getOffset();
     }
 }
